38dbe7a7fe9ebb786e12a20e1d41da8be4b9c628
Maciej Kaznowski <mmkaznowski@gmail.com>
2020-04-17 20:17:01 +0100

    Expose Chars

--- src/main/kotlin/com/innercirclesoftware/randoms/RandomObjects.kt
+++ src/main/kotlin/com/innercirclesoftware/randoms/RandomObjects.kt
@@ -2,10 +2,15 @@ package com.innercirclesoftware.randoms
 
 import kotlin.random.Random
 
-private val DIGITS = CharRange(start = '0', endInclusive = '9')
-private val LOWERCASE_LETTERS_ASCII = CharRange(start = 'a', endInclusive = 'z')
-private val UPPERCASE_LETTERS_ASCII = CharRange(start = 'A', endInclusive = 'Z')
-private val ALPHA_NUMERIC = (DIGITS + LOWERCASE_LETTERS_ASCII + UPPERCASE_LETTERS_ASCII).toCharArray()
+object Chars {
+
+    val DIGITS = CharRange(start = '0', endInclusive = '9')
+    val LOWERCASE_LETTERS_ASCII = CharRange(start = 'a', endInclusive = 'z')
+    val UPPERCASE_LETTERS_ASCII = CharRange(start = 'A', endInclusive = 'Z')
+    val ALPHA_NUMERIC = (DIGITS + LOWERCASE_LETTERS_ASCII + UPPERCASE_LETTERS_ASCII).toCharArray()
+    val ALL = CharRange(start = Char.MIN_VALUE, endInclusive = Char.MAX_VALUE)
+
+}
 
 /**
  * Returns a [String] of [size] consisting of characters in [chars]
@@ -16,7 +21,7 @@ private val ALPHA_NUMERIC = (DIGITS + LOWERCASE_LETTERS_ASCII + UPPERCASE_LETTER
  */
 fun randomString(
     size: Int = randomInt(min = 0, maxInclusive = 31),
-    chars: CharArray = ALPHA_NUMERIC,
+    chars: CharArray = Chars.ALPHA_NUMERIC,
     seed: Random = Seeds.default
 ): String {
     require(size >= 0) { "Size must be non-negative: $size" }
